9bf606700bf99bffe5a988c0a553749a5934b2b4
Tejasi <31427342+tejasipalkar@users.noreply.github.com>
2018-04-03 20:26:40 -0700

    Update Console.java
    initial version

--- src/view/server/components/Console.java
+++ src/view/server/components/Console.java
@@ -2,16 +2,46 @@ package view.server.components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Console {
 
+    /**
+     * @wbp.parser.entryPoint
+     */
     public static JPanel getPanel() {
         JPanel console = new JPanel();
 
-        JLabel instructions = new JLabel("Server console, with clear button", JLabel.CENTER);
-        console.add(instructions);
-
-        console.setPreferredSize(new Dimension(600, 150));
+        console.setPreferredSize(new Dimension(600, 190));
+        console.setLayout(null);
+        
+        JLabel consoleLabel = new JLabel("Console");
+        consoleLabel.setBounds(15, -1, 119, 27);
+        consoleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        console.add(consoleLabel);
+        
+        JTextPane consoleTextPane = new JTextPane();
+        consoleTextPane.setBounds(15, 30, 538, 103);
+        consoleTextPane.setForeground(new Color(255, 0, 51));
+        consoleTextPane.setBackground(new Color(255, 255, 255));
+        console.add(consoleTextPane);
+        
+        JButton clrLogBtn = new JButton("ClearLog");
+        clrLogBtn.setBounds(15, 147, 144, 27);
+        clrLogBtn.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        clrLogBtn.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        	}
+        });
+        console.add(clrLogBtn);
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(556, 30, 29, 103);
+        console.add(scrollPane);
+        
+        JScrollBar scrollBarConsole = new JScrollBar();
+        scrollPane.setViewportView(scrollBarConsole);
         return console;
     }
 }
