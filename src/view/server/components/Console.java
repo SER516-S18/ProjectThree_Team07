37397f09ed53548de48ab235919faf68ccb4f9e1
Abhishek Parmar <35242470+AbhishekParmar81@users.noreply.github.com>
2018-04-06 10:59:59 -0700

    Changed Clear Log and Message Methods

--- src/view/server/components/Console.java
+++ src/view/server/components/Console.java
@@ -1,6 +1,7 @@
 package view.server.components;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
@@ -13,16 +14,15 @@ import java.util.logging.Logger;
 import java.awt.event.ActionEvent;
 
 public class Console {
-	
+
 	private static JPanel console = null;
 	private final static Logger LOGGER = Logger.getLogger(Console.class.getName());
 	private final static String CONTENT_TYPE = "text/html";
+	private static JTextPane consoleTextPane = null;
 
-	
-    /**
-     * @wbp.parser.entryPoint
-     */
-	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
 	public static JPanel getConsolePanel()
 	{
 		if(console == null)
@@ -31,82 +31,57 @@ public class Console {
 		}
 		return console;
 	}
-	
-    public static void getPanel() {
-        console = new JPanel();
 
-        console.setPreferredSize(new Dimension(600, 190));
-        console.setLayout(null);
-        
-        JLabel consoleLabel = new JLabel("Console");
-        consoleLabel.setBounds(15, -1, 119, 27);
-        consoleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        console.add(consoleLabel);
-          
-        JTextPane consoleTextPane = new JTextPane();
-        consoleTextPane.setBounds(15, 30, 538, 103);
-        consoleTextPane.setForeground(new Color(255, 0, 51));
-        consoleTextPane.setBackground(new Color(255, 255, 255));
-      //  console.add(consoleTextPane);
-        
-        JButton clrLogBtn = new JButton("ClearLog");
-        clrLogBtn.setBounds(15, 147, 144, 27);
-        clrLogBtn.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        clrLogBtn.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent arg0) {
-			consoleTextPane.setText("");
-        	}
-        });
-        console.add(clrLogBtn);
-        
-        JScrollPane scrollPane = new JScrollPane(consoleTextPane);
-        scrollPane.setBounds(15, 30, 538, 103);
-        console.add(scrollPane);
-        
-//        JScrollBar scrollBarConsole = new JScrollBar();
-//        scrollPane.setViewportView(scrollBarConsole);
-       // return console;
-    }
-   
-    
+	public static void getPanel() {
+		console = new JPanel();
+
+		console.setPreferredSize(new Dimension(600, 190));
+		console.setLayout(null);
+
+		JLabel consoleLabel = new JLabel("Console");
+		consoleLabel.setBounds(15, -1, 119, 27);
+		consoleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		console.add(consoleLabel);
+
+		if(consoleTextPane == null) {
+			consoleTextPane = new JTextPane();
+			consoleTextPane.setBounds(15, 30, 538, 103);
+			consoleTextPane.setForeground(new Color(255, 0, 51));
+			consoleTextPane.setBackground(new Color(255, 255, 255));
+		}
+
+		JScrollPane scrollPane = new JScrollPane(consoleTextPane);
+		scrollPane.setBounds(15, 30, 538, 103);
+		console.add(scrollPane);
+
+		JButton clrLogBtn = new JButton("ClearLog");
+		clrLogBtn.setBounds(15, 147, 144, 27);
+		clrLogBtn.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		clrLogBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				consoleTextPane.setContentType(CONTENT_TYPE);
+				StyledDocument doc = (StyledDocument) consoleTextPane.getDocument();
+				try {
+					doc.remove(0, doc.getLength());
+					//doc.insertString(0,null,null );
+				} catch (BadLocationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		console.add(clrLogBtn);
+	}
+
 	public static void setErrorMessage(String errorMessage) {
-
 		try
 		{
-			console = new JPanel();
-
-	        console.setPreferredSize(new Dimension(600, 190));
-	        console.setLayout(null);
-
-	        JLabel consoleLabel = new JLabel("Console");
-	        consoleLabel.setBounds(15, -1, 119, 27);
-	        consoleLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-	        console.add(consoleLabel);
-	       
-	        JTextPane consoleTextPane = new JTextPane();
-	        
+			consoleTextPane = new JTextPane();
 			consoleTextPane.setContentType(CONTENT_TYPE);
 			StyledDocument doc = (StyledDocument) consoleTextPane.getDocument();
 			SimpleAttributeSet keyWord = new SimpleAttributeSet();
 			StyleConstants.setForeground(keyWord, Color.RED);
-			StyleConstants.setFontFamily(keyWord, "Times New Roman");
-			StyleConstants.setFontSize(keyWord, 9);
-			StyleConstants.setBackground(keyWord, Color.LIGHT_GRAY);
-
 			doc.insertString(0,new Date()+" - Error - "+errorMessage +"\n",keyWord );
-			
-			JButton clrLogBtn = new JButton("ClearLog");
-	        clrLogBtn.setBounds(15, 147, 144, 27);
-	        clrLogBtn.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-	        clrLogBtn.addActionListener(new ActionListener() {
-	        	public void actionPerformed(ActionEvent arg0) {
-	        	}
-	        });
-	        console.add(clrLogBtn);
-	        
-	        JScrollPane scrollPane = new JScrollPane(consoleTextPane);
-	        scrollPane.setBounds(15, 30, 538, 103);
-	        console.add(scrollPane);
 		}
 		catch(Exception ex) 
 		{ 
@@ -116,30 +91,18 @@ public class Console {
 	}
 
 	/**
-	 * sets the layout for normal messages
-	 * @param message
+	 * sets the layout and normal messages
+	 * @param message to be displayed
 	 */
 	public static void setMessage(String message) {
 		try {
-			console = new JPanel();
-
-	        console.setPreferredSize(new Dimension(600, 190));
-	        console.setLayout(null);
-
-	        JTextPane consoleTextPane = new JTextPane();
-
+			consoleTextPane = new JTextPane();
 			consoleTextPane.setContentType(CONTENT_TYPE);
 			StyledDocument doc = (StyledDocument) consoleTextPane.getDocument();
-			SimpleAttributeSet keyWord = new SimpleAttributeSet();
-			StyleConstants.setForeground(keyWord, Color.BLACK);
-			StyleConstants.setFontFamily(keyWord, "Times New Roman");
-			StyleConstants.setFontSize(keyWord, 9);
-			StyleConstants.setBackground(keyWord, Color.LIGHT_GRAY);
-			doc.insertString(0,new Date()+"- Message - "+message+"\n",keyWord );
+			doc.insertString(0,new Date()+"- Message - "+message+"\n",null );
 		} catch (Exception ex) {
 			LOGGER.log(Level.SEVERE,"Exception while adding Message", ex);
 		}
 	}
-
 
 }
