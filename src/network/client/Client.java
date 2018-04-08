46845af607e272f473f34eec6d384037bce897c4
isidro <idperez@asu.edu>
2018-04-08 15:46:48 -0700

    singleton client

--- src/network/client/Client.java
+++ src/network/client/Client.java
@@ -5,36 +5,31 @@ import org.glassfish.tyrus.client.ClientManager;
 import javax.websocket.DeploymentException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.concurrent.CountDownLatch;
 
 import static util.NetworkConstants.SERVER_URI;
 
 public class Client {
-    private static CountDownLatch latch;
 
     private static ClientManager clientManager;
 
-    public Client() {
-        clientManager = ClientManager.createClient();
+    private static Client clientInstance;
+
+    private Client() {}
+
+    public static Client getInstance() {
+        if(clientInstance == null) {
+            clientManager = ClientManager.createClient();
+            clientInstance = new Client();
+        }
+        return clientInstance;
     }
 
-    public  void start() {
-        latch = new CountDownLatch(1);
-
+    public void start() {
         try {
             clientManager.connectToServer(ClientEndpoint.class, new URI(SERVER_URI));
-            latch.await();
-
         } catch (DeploymentException | URISyntaxException e) {
             throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-
-    }
-
-    public static void main(String[] args) {
-       new  Client().start();
     }
 }
 
