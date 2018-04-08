e9b3c95c00282dab0868d48e4baae72bc53854cf
isidro <idperez@asu.edu>
2018-04-08 12:58:43 -0700

    singleton timer class

---
+++ src/controller/server/MessageTimer.java
@@ -0,0 +1,54 @@
+package controller.server;
+
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * A Timer singleton that runs repetitively for the specified interval
+ *
+ */
+public class MessageTimer {
+	private boolean isAutoRepeated = false;
+	private Timer timer;
+
+	private static MessageTimer timerInstance = null;
+
+	private MessageTimer() {}
+
+	public static MessageTimer getInstance() {
+		if (timerInstance == null) {
+			timerInstance = new MessageTimer();
+		}
+		return timerInstance;
+	}
+
+	/**
+	 * Constructs and starts the timer thread
+	 * 
+	 * @param interval - interval in which data has to sent to the client
+	 */
+	public void startTimer(float interval) {
+		timer = new Timer( Math.round(interval * 1000), new ActionListener() {
+			public void actionPerformed( ActionEvent event )  {
+
+			}
+		});
+		timer.setRepeats(this.isAutoRepeated);
+		timer.start();
+	}
+	
+	/**
+	 * Stops the timer thread
+	 */
+	public void stopTimer() {
+		timer.stop();
+	}
+
+	/**
+	 * Sets if the timer should repeat or not
+	 */
+	public void setAutoRepeated(boolean autoRepeated) {
+		this.isAutoRepeated = autoRepeated;
+	}
+}
